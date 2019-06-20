8276ea1f411bc9ade1bb779658d8d1f69f4d1f17
zhanglianzhong <qinghaihudream@163.com>
2019-06-20 23:17:16 +0800

    新建springboot工程

---
+++ src/main/java/com/uestc/springbootadminlteadmin/SpringbootAdminlteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.uestc.springbootadminlteadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminlteAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminlteAdminApplication.class, args);
+	}
+
+}
